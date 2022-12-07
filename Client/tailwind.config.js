e1eff03a92c05e4db0020d9420357bf025c53535
kavinkumar <kavinkumars218@gmail.com>
2022-12-07 18:07:36 +0530

    added loader and icon in head tag and changed font in home page

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        marker: ['Permanent Marker'],
+      },
+    },
   },
   plugins: [],
 }
