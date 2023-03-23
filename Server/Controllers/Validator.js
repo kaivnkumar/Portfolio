3785c8e28747fc827c97e3834285b446ba650eea
kavinkumar <kavinkumars218@gmail.com>
2023-03-23 22:48:53 +0530

    edited samesite setting

--- Server/Controllers/Validator.js
+++ Server/Controllers/Validator.js
@@ -12,12 +12,13 @@ export const Validate = async (req, res) => {
     const validatePassword = await bcrypt.compare(Password, data.Password);
     if (validatePassword) {
       const token = await tokenGeneratore("admin");
-      res.cookie("jwt", token,
-        {expires: new Date(Date.now() + 300000) },
-        {secure: true},
-        {httpOnly: true},
-        {sameSite: 'none'},
-        {path: '/'},
+      res.cookie("jwt", token, {
+        expires: new Date(Date.now() + 300000),
+        secure: true,
+        httpOnly: true,
+        sameSite: 'none',
+        path: '/',
+      }
         // {domain: '.vercel.app'}
         );
       res.send({
