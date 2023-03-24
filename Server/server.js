fbf886d8915c0ed4173513ee38ea0d2af9f0b1ee
kavinkumar <kavinkumars218@gmail.com>
2023-03-24 16:06:31 +0530

    changed vercel file1

--- Server/server.js
+++ Server/server.js
@@ -13,6 +13,8 @@ const app = Express();
 
 const PORT = process.env.PORT;
 
+app.use(express.static('public'));
+
 // app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cors({ credentials: true, origin:"https://kavinkumar.vercel.app"}));
 
